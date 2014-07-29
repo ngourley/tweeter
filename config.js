01777959a32f20c66ddcdc48b90ecf0ee8bb22e4
Nathan Gourley <ngourley@gmail.com>
2014-07-28 21:08:49 -0500

    Add getUri method to config.js

--- config.js
+++ config.js
@@ -18,6 +18,12 @@ config.mongo.database = process.env.MONGO_DATABASE || 'twitter';
 config.mongo.host = process.env.MONGO_HOST || 'kahana.mongohq.com';
 config.mongo.port = process.env.MONGO_PORT || '10078';
 
+config.mongo.getUri = function() {
+    return 'mongodb://' + config.mongo.user + ':' +
+        config.mongo.password + '@' + config.mongo.host +
+        ':' + config.mongo.port + '/' + config.mongo.database;
+};
+
 config.winston = {};
 config.winston.level = 'debug';
 config.winston.colorize = true;
