1cfbbbef9dff441d187dea31714a3a44add6d4b9
Nathan Gourley <ngourley@gmail.com>
2014-07-28 20:55:46 -0500

    Add mongodb options to config.js

--- config.js
+++ config.js
@@ -3,13 +3,20 @@ var config = {};
 
 config.twitter = {};
 config.twitter.api = {
-    consumer_key: ''
-  , consumer_secret: ''
-  , access_token: ''
-  , access_token_secret: ''
+      consumer_key: ''
+    , consumer_secret: ''
+    , access_token: ''
+    , access_token_secret: ''
 };
 
 config.twitter.username = 'nathangourley';
+
+config.mongo = {};
+config.mongo.user = process.env.MONGO_USER || 'root';
+config.mongo.password = process.env.MONGO_PASSWORD || 'password';
+config.mongo.database = process.env.MONGO_DATABASE || 'twitter';
+config.mongo.host = process.env.MONGO_HOST || 'kahana.mongohq.com';
+config.mongo.port = process.env.MONGO_PORT || '10078';
 
 config.winston = {};
 config.winston.level = 'debug';
