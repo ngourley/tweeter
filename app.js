cb497990973102102b96677192d310ef627ba1d5
Nathan Gourley <ngourley@gmail.com>
2014-08-09 23:02:44 -0500

    Remove commented code in app.js from previous testing.

--- app.js
+++ app.js
@@ -33,30 +33,3 @@ port = process.env.PORT || 3000;
 server.listen(port, function() {
     winston.info('Express listening on ' + port);
 });
-
-// bot.retweetTopic('#angularjs', function (err, data) {
-//     console.log(err)
-//     console.log(data)
-// });
-
-// bot.retweetRandom(function (err, data) {
-//     console.log(err);
-//     console.log(data);
-// });
-
-// bot.printMyTweets(function(err, tweets) {})
-// bot.untweet({id_str:'490275390292631552'}, function(err, data){
-//     console.log(err)
-// })
-
-// bot.mingleLikelyToFollow(function (err, user) {
-//     if (err) return winston.error(err);
-//     console.log('Now following @' + user.screen_name);
-// });
-// setInterval(function(){
-//     bot.mingleLikelyToFollow(function (err, user) {
-//         if (err) return winston.error(err);
-//         console.log('Now following @' + user.screen_name);
-//     });
-// }, moment.duration(20, 'minutes'));
-
