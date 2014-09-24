d5c8fffaead0caa090a1b89f799c621afc326340
Nathan Gourley <ngourley@gmail.com>
2014-09-23 20:24:42 -0500

    Merge unstable changes for config.js

--- config.js
+++ config.js
@@ -11,6 +11,19 @@ config.twitter.api = {
 
 config.twitter.username = 'nathangourley';
 
+config.mongo = {};
+config.mongo.user = process.env.MONGO_USER || 'system';
+config.mongo.password = process.env.MONGO_PASSWORD || 'Cappie2014!';
+config.mongo.database = process.env.MONGO_DATABASE || 'twitter';
+config.mongo.host = process.env.MONGO_HOST || 'kahana.mongohq.com';
+config.mongo.port = process.env.MONGO_PORT || '10078';
+
+config.mongo.getUri = function() {
+    return 'mongodb://' + config.mongo.user + ':' +
+        config.mongo.password + '@' + config.mongo.host +
+        ':' + config.mongo.port + '/' + config.mongo.database;
+};
+
 config.winston = {};
 config.winston.level = 'debug';
 config.winston.colorize = true;
